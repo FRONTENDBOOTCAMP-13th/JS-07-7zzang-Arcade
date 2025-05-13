7d431fd4288ecf2b1140064bc18076b11720b524
YouVin <dbqls990909@naver.com>
2025-05-14 02:06:36 +0900

    ✨ feature : Initialize game setting, player move #5

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
       input: {
         index: 'index.html', // 기본 index.html
         // 필요한 다른 HTML 파일을 여기에 추가
+        space: 'src/pages/the-seventh-space/space.html',
       },
     },
   },
