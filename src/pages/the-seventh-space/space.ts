af4e9324ffe02feced75cc0f92d8d0df7179b73a
YouVin <dbqls990909@naver.com>
2025-05-14 15:33:21 +0900

    ✨ feature : player shoots #9

--- src/pages/the-seventh-space/space.ts
+++ src/pages/the-seventh-space/space.ts
@@ -18,6 +18,9 @@ interface IGameObject {
 interface IPlayer extends IGameObject {
   speed: number;
   sprite: HTMLImageElement;
+  bullets: IBullet[];
+  shoot(): void;
+  updateBullets(): void;
 }
 
 interface IEnemy extends IGameObject {
@@ -39,6 +42,11 @@ interface IEnemyConfig {
   descentY: number;
 }
 
+interface IBullet extends IGameObject {
+  speedY: number;
+  sprite: HTMLImageElement;
+}
+
 // ─── 객체 정의 ────────────────────────────────────────────────────
 // 1) 플레이어
 const Player: IPlayer = {
@@ -48,13 +56,55 @@ const Player: IPlayer = {
   height: 60,
   speed: 4,
   sprite: new Image(),
+  bullets: [],
 
   update() {
     if (keys.ArrowLeft && this.x > 0) this.x -= this.speed;
     if (keys.ArrowRight && this.x + this.width < canvas.width) this.x += this.speed;
+
+    this.updateBullets();
   },
   draw(ctx) {
+    // ──Player 그리기 ─────────────────────────────────────────────────
     ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+
+    // ──Bullet 그리기 ─────────────────────────────────────────────────
+    for (let i = 0; i < this.bullets.length; i++) {
+      this.bullets[i].draw(ctx);
+    }
+  },
+
+  shoot() {
+    // 1. 총알 좌표 잡기
+    // x 좌표는 플레이어의 딱 중간 위치 따라서 width / 2 절반
+    // y 좌표는 플레이어의 머리 위치 y
+    const bulletX = this.x + this.width / 2 - 10;
+    const bulletY = this.y;
+
+    // 2. 총알 생성하기 x좌표, y좌표, 넓이, 높이, 스피드, 이미지, update, draw
+    this.bullets.push({
+      x: bulletX,
+      y: bulletY,
+      width: 20,
+      height: 20,
+      speedY: 3,
+      sprite: bulletImg,
+      // update 함수에는 총알이 이동하는 모습을 구현
+      update() {
+        // 현재 y 위치에서 스피드 만큼 빼서 위로 올라가도록 구현
+        this.y = this.y - this.speedY;
+      },
+      draw() {
+        // ctx.drawImage(이미지, x, y, width, height);
+        ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+      },
+    });
+  },
+
+  updateBullets() {
+    for (let i = 0; i < this.bullets.length; i++) {
+      this.bullets[i].update();
+    }
   },
 };
 
@@ -129,12 +179,18 @@ const playerImg = new Image();
 playerImg.src = '../../assets/images/space-img/player.png';
 Player.sprite = playerImg;
 
+const bulletImg = new Image();
+bulletImg.src = '../../assets/images/space-img/player-bullet.png';
+
 const backgroundImg = new Image();
 backgroundImg.src = '../../assets/images/space-img/background.png';
 
 const keys: Record<'ArrowLeft' | 'ArrowRight', boolean> = { ArrowLeft: false, ArrowRight: false };
 document.addEventListener('keydown', e => {
   if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') keys[e.key] = true;
+  if (e.code === 'Space') {
+    Player.shoot();
+  }
 });
 document.addEventListener('keyup', e => {
   if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') keys[e.key] = false;
