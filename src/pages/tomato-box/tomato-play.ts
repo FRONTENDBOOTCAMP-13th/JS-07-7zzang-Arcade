afdf8b86085bf43ee699fe6ac8f6e2e55260423b
kimlog126 <jisu11260@gmail.com>
2025-05-15 19:58:24 +0900

    ✨ feature: tomato box time, gameover, savescore UI #17

--- src/pages/tomato-box/tomato-play.ts
+++ src/pages/tomato-box/tomato-play.ts
@@ -6,9 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const numWidth = canvas.width / cols;
   const numHeight = canvas.height / rows;
 
+  const currTimeBar = document.querySelector('.curr') as HTMLDivElement;
+  const timeLimit = 10;
+  let timeLeft = timeLimit;
+
   const tomatoImage = new Image();
   tomatoImage.src = '/src/assets/images/tomato-img/tomato-empty.png';
 
+  // 랜덤 숫자 생성
   Promise.all([document.fonts.ready, new Promise(resolve => (tomatoImage.onload = resolve))]).then(() => {
     ctx.font = `${numHeight * 0.6}px DungGeunMo`;
     ctx.fillStyle = '#FFD700';
@@ -30,4 +35,53 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+
+  // 시간 제한
+  function startTimer() {
+    const timer = setInterval(() => {
+      timeLeft--;
+
+      const per = (timeLeft / timeLimit) * 70;
+      currTimeBar.style.height = `${per}%`;
+
+      if (timeLeft <= 0) {
+        clearInterval(timer);
+
+        setTimeout(() => {
+          currTimeBar.style.height = `0%`;
+          gameOver();
+        }, 1000);
+      }
+    }, 1000);
+  }
+
+  // 개임 오버
+  function gameOver() {
+    const gameOverScreen = document.querySelector('.gameover') as HTMLDivElement;
+    if (gameOverScreen) {
+      gameOverScreen.classList.add('show');
+    }
+  }
+
+  // 점수확인
+  const saveBtn = document.querySelector('.savebtn');
+  const saveScore = document.querySelector('.savescore') as HTMLElement;
+
+  let isVisible = false;
+
+  saveBtn?.addEventListener('click', () => {
+    saveToggle(saveScore);
+  });
+
+  function saveToggle(saveEl: HTMLElement) {
+    if (!isVisible) {
+      saveEl.classList.remove('hide');
+      saveEl.classList.add('show');
+    } else {
+      saveEl.classList.remove('show');
+      saveEl.classList.add('hide');
+    }
+    isVisible = !isVisible;
+  }
+  startTimer();
 });
